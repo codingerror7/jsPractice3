9149b0e4f836ed943cc810b5e5b1a7abc8b5c5a9
CodingError7 <144795507+codingerror7@users.noreply.github.com>
2025-03-26 01:33:33 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,186 @@
+//console.log("Hello world, This is webdevelopment terminal!!");
+
+//function employee(name) {
+    //  console.log(name + " Good Morning");
+    //}
+    
+//let name = "Raman";
+//let name1 = "Aman";
+//let name2 = "anaconda";
+//let name3 = "avengers";
+//let name4 = "Iron Man";
+//let name5 = "captain america"
+//let name6 = "Black sword widow";
+//let name7 = "Shaktimaann";
+//let name8 = "hero the one and only";
+//let name9 = "oreo the king of jungle";
+//let name10 = "oreo has fear from a cat";
+
+//employee(name);
+//employee(name1);
+//employee(name2);
+//employee(name3);
+//employee(name4);
+//employee(name5);
+//employee(name6);
+//employee(name7);
+//employee(name8);
+//employee(name9);
+//employee(name10);
+
+
+//to give two variables in a function:
+
+//function employee(greeting,name) { 
+  //   console.log(greeting + name);
+//}
+
+
+//let name = "Raman";
+//let name1 = "aman";
+//let name2 = "aliens";
+//let name3 = "oreo the rabbit";
+//let name4 = "black cobra";
+//let name5 = "Illuminati";
+//let name6 = "Black Hats";
+//let name7 = "Grey Hats";
+//let name8 = "White hat";
+//let name9 = "Hackers and their groups";
+//let name10 = "Anonymous";
+//let name11 = "Inverted hacking encyclopedia";
+
+//let greeting = "Good Morning, This Is ";
+
+//employee(greeting,name);
+//employee(greeting,name1);
+//employee(greeting,name2);
+//employee(greeting,name3);
+//employee(greeting,name4);
+//employee(greeting,name5);
+//employee(greeting,name6);
+//employee(greeting,name7);
+//employee(greeting,name8);
+//employee(greeting,name9);
+//employee(greeting,name10);
+//employee(greeting,name11);
+
+
+//INPUT FUNCTION TO CHEXK ELIGIBILITY TO VOTE:
+//function adult(a) {
+  //if(a >= 18) {
+    //console.log("YOU ARE ELIGIBLE TO VOTE BCOZ YOU ARE AN ADULT!");
+ // }
+ //else {
+  //console.log("YOU ARE NOT ELIGIBLE TO VOTE BCOZ YOU ARE NOT AN ADULT!!!");
+// }
+//}
+
+//adult(a);
+
+
+//function of an array:
+
+//console.log("YOUR DETAILS ARE:")
+//function list() {
+  //  let employee = {
+    //    name : "Sujal Saraswat",
+    //    ID : 123456,
+    //    Profession : "Software Engineer",
+    //    Salary : 12 + " Lacs Per annum",
+    //    "Email ID" : "Saraswatsujal@Gmail.com",
+    //    City : "Bhopal",
+    //    state : "Madhya Pradesh",
+    //    Country : "India",
+    //    "Pin Code" : 463106,
+    //    Education : "BTech CSE",
+    //    College : "Jai Narayan College Of Technology",
+    //    Gender : "Male"
+    //}
+    //console.log(employee);
+//}
+//calling the function:
+//list();
+
+//another way of writing a function:
+
+//function sum(a , b) {
+  //return a + b;
+//}
+
+//function multy(a , b) {
+  //return a * b;
+//}
+
+//function main(a,b) {
+  //console.log(a + b)
+//}
+
+//main(a,b);
+
+
+//function main(a) {
+  //if(a >= 18) {
+    //console.log("YOU CAN VOTE AS YOU ARE AN ADULT");
+//  }
+//else{
+  //console.log("YOU CANNOT VOTE AS YOU ARE NOT AN ADULT!!!");
+//}
+//}
+
+//main(a);
+
+//ARROW FUNCTION:
+
+//let a = (a,b) => {
+  //console.log(a + b);
+//}
+
+
+//let age = (a) => {
+  //if(a >= 18) {
+    //console.log("YOU ARE AN ADULT SO IT IS YOUR DUTY TO PROTECT THE COUNTRY!");
+//  }
+     //else {
+      //console.log("YPOU ARE NOT AN ADULT SO IT IS YOUR DUTY TO PROTECT THE COUNTRY FROM ADULTS!!!");
+    // }
+//}
+
+
+//functions to print table of given number input by the user:
+
+//function main(a) {
+  //console.log("YOUR PRINTED TABLE:");
+  //for(let i = 0; i <= 10; i++) {
+    //console.log(a*i);
+//  }
+//}
+//main(a);
+
+//FUNCTION FOR TABLE OF NUMBER GIVEN BY USER:
+//function main(a) {
+  //for(let i = 0; i <= 10; i++) {
+    //console.log(a*i);
+//  }
+//}
+
+//main(a);
+
+
+//FUNCTION FOR INPUT OF AGE BY THE USER AND ALLOWING IT TO ACCESS THE NETWORK:
+//let a = prompt("PLEASE ENTER YOUR AGE BEFORE ENTERING THE NETWORK!");
+//function age(a) {
+  //if(a >= 18) {
+    //console.log("YOU CAN ACCESS THE NETWORK AS YOU ARE ADULT!");
+//  }
+//else {
+  //console.log("YOU CANNOT ACCESS THE NETWORK BCOZ YOU ARE BELOW 18!");
+//}
+//}
+
+//main(a);
+
+
+//TO TAKE INPUT FROM THE USER:
+//function numb(a , b) {
+  //return a , b;
+//}
